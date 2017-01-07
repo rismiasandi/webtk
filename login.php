712e5639e7d59416640f4e2020908a243a39a356
family-PC\family <sofiyanidewi08@gmail.com>
2017-01-07 12:44:08 +0700

    MENGUBAH TITLE LOGIN

--- login.php
+++ login.php
@@ -30,7 +30,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>about - Business Casual - Start Bootstrap Theme</title>
+    <title>lOGIN</title>
 
     <!-- Bootstrap Core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
