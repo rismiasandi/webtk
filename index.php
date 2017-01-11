7c24743ad38d3607d38feb27e4a61f17c3b63e44
USER-PC\USER <rismiasandi63@gmail.com>
2017-01-11 14:24:04 +0700

    Home jadi bold dan gak bisa di clik kalau sudah di home

--- index.php
+++ index.php
@@ -64,7 +64,7 @@ include("koneksi.php");
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav">
                     <li>
-                        <a href="index.php">HOME</a>
+                        <a href=#><b>HOME</a></b>
                     </li>
                     <li>
                         <a href="about.php">SEJARAH</a>
